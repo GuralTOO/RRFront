a5bf65de01cae4d7d1b2cb0a7f724cc3a4b689d8
Gural Nuriyev <guraltoo@Gurals-MacBook-Pro.local>
2025-01-16 16:53:54 -0500

    default to dark mode

--- src/Scripts/LandingPage/components/ThemeContext.jsx
+++ src/Scripts/LandingPage/components/ThemeContext.jsx
@@ -1,20 +1,20 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext({
-  theme: 'light',
+  theme: 'dark',
   setTheme: () => null,
 });
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     // Check for saved theme preference
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
       setTheme(savedTheme);
-    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-      setTheme('light');
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
     }
   }, []);
 
